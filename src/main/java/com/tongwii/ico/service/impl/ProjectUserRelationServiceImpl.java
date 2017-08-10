fcfd8c8d3856a6e53a77310efcfc26d2a8f7e896
yamo <2318798407@qq.com>
2017-08-10 20:16:30 +0800

    新增根据用户id查询锁定项目接口

--- src/main/java/com/tongwii/ico/service/impl/ProjectUserRelationServiceImpl.java
+++ src/main/java/com/tongwii/ico/service/impl/ProjectUserRelationServiceImpl.java
@@ -1,13 +1,19 @@
 package com.tongwii.ico.service.impl;
 
 import com.tongwii.ico.dao.ProjectUserRelationMapper;
+import com.tongwii.ico.model.Project;
 import com.tongwii.ico.model.ProjectUserRelation;
+import com.tongwii.ico.service.ProjectService;
 import com.tongwii.ico.service.ProjectUserRelationService;
 import com.tongwii.ico.core.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,5 +24,23 @@ import javax.annotation.Resource;
 public class ProjectUserRelationServiceImpl extends AbstractService<ProjectUserRelation> implements ProjectUserRelationService {
     @Resource
     private ProjectUserRelationMapper projectUserRelationMapper;
+    @Autowired
+    private ProjectService projectService;
 
+    @Override
+    public List<Project> findByUserId(Integer userId) {
+        // 首先先获取项目Id
+        ProjectUserRelation projectUserRelation = new ProjectUserRelation();
+        projectUserRelation.setUserId(userId);
+        List<ProjectUserRelation> projectUserRelations = projectUserRelationMapper.select(projectUserRelation);
+        if(CollectionUtils.isEmpty(projectUserRelations)){
+            return null;
+        }
+        List<Project> projectList = new ArrayList<>();
+        for (int i=0; i<projectUserRelations.size(); i++){
+            Project project = projectService.findById(projectUserRelations.get(i).getProjectId());
+            projectList.add(project);
+        }
+        return projectList;
+    }
 }
