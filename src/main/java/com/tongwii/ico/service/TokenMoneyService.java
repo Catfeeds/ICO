4ae71064794bb33f06f5c3af6a4f7a878ef7b136
yamo <2318798407@qq.com>
2017-08-11 10:38:32 +0800

    新增根据代码比名称查询代币信息接口 修改项目查询接口

--- src/main/java/com/tongwii/ico/service/TokenMoneyService.java
+++ src/main/java/com/tongwii/ico/service/TokenMoneyService.java
@@ -7,5 +7,10 @@ import com.tongwii.ico.core.Service;
  * Created by Zeral on 2017-08-02.
  */
 public interface TokenMoneyService extends Service<TokenMoney> {
-
+    /**
+     * 根据代币名称查询代币信息
+     * @param ICOname
+     * @return
+     */
+    TokenMoney findByName(String ICOname);
 }
