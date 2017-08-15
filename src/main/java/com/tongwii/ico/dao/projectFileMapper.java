db2719172a85431d1fc0950339d1af0d28ecce60
zeral <zeralzhang@gmail.com>
2017-08-15 11:26:22 +0800

    上传消息实体，文件实体

---
+++ src/main/java/com/tongwii/ico/dao/projectFileMapper.java
@@ -0,0 +1,7 @@
+package com.tongwii.ico.dao;
+
+import com.tongwii.ico.core.Mapper;
+import com.tongwii.ico.model.projectFile;
+
+public interface projectFileMapper extends Mapper<projectFile> {
+}
---
+++ src/main/java/com/tongwii/ico/service/impl/projectFileServiceImpl.java
@@ -0,0 +1,21 @@
+package com.tongwii.ico.service.impl;
+
+import com.tongwii.ico.core.AbstractService;
+import com.tongwii.ico.dao.projectFileMapper;
+import com.tongwii.ico.model.projectFile;
+import com.tongwii.ico.service.projectFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * Created by Zeral on 2017-08-15.
+ */
+@Service
+@Transactional
+public class projectFileServiceImpl extends AbstractService<projectFile> implements projectFileService {
+    @Autowired
+    private projectFileMapper fileMapper;
+
+}
