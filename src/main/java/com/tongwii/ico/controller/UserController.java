5e8655e9827e5b48943f00a1f05b4df42d513112
karen <754866223@qq.com>
2017-08-18 10:55:14 +0800

    修改用户修改

--- src/main/java/com/tongwii/ico/controller/UserController.java
+++ src/main/java/com/tongwii/ico/controller/UserController.java
@@ -82,7 +82,7 @@ public class UserController {
     @ResponseBody
     public Result update(@RequestBody User user) {
         //设置身份证信息的唯一性更改
-        if(user.getIdCard() != null || !user.getIdCard().isEmpty()){
+        if(user.getIdCard() != null && !user.getIdCard().isEmpty()){
             //校验身份证是否在数据库中存在
             if(userService.findByIdCard(user.getIdCard()) != null){
                 return Result.failResult("身份证信息已存在!");
