635396987ca72ecca4bec9db7d7f72bad86c0dc0
yamo <2318798407@qq.com>
2017-08-28 17:27:32 +0800

    新增忘记密码功能

--- src/main/java/com/tongwii/ico/controller/AuthController.java
+++ src/main/java/com/tongwii/ico/controller/AuthController.java
@@ -8,6 +8,8 @@ import com.tongwii.ico.security.JwtUser;
 import com.tongwii.ico.service.UserService;
 import com.tongwii.ico.service.UserWalletService;
 import com.tongwii.ico.util.ContextUtils;
+import com.tongwii.ico.util.MessageUtil;
+import com.tongwii.ico.util.ValidateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +21,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 授权controller
@@ -45,6 +51,10 @@ public class AuthController {
     private UserService userService;
     @Autowired
     private UserWalletService userWalletService;
+    @Autowired
+    private MessageUtil messageUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     /**
      * 创建身份验证token
@@ -96,6 +106,74 @@ public class AuthController {
             return Result.successResult().add( "token", refreshedToken );
         } else {
             return Result.failResult( "token 无法刷新,未认证通过" );
+        }
+    }
+
+    /**
+     * 忘记密码
+     * 根据用户邮箱与手机号判断用户是否存在
+     */
+    @GetMapping("/userExist/{emailAccount}&{phone}")
+    @ResponseBody
+    public Result userExist(@PathVariable(value = "emailAccount") String emailAccount, @PathVariable(value = "phone") String phone){
+        try {
+            User u = userService.findByUsername(emailAccount);
+            if(u!=null){
+                if(u.getPhone()!=null){
+                    if(u.getPhone().equals(phone)){
+                        return Result.successResult("匹配成功!").add("userInfo",u);
+                    }else{
+                        return Result.errorResult("邮箱与手机号码不匹配，如有疑问请联系客服!");
+                    }
+                }else{
+                    return Result.errorResult("您未手机认证，请联系客服进行修改!");
+                }
+            }else{
+                return Result.errorResult("该邮箱未注册!");
+            }
+        }catch (Exception e){
+            return Result.errorResult("邮箱与电话号码不可为空!");
+        }
+    }
+
+    /**
+     * 忘记密码——》验证手机号
+     */
+    @PostMapping("/validatePhone")
+    @ResponseBody
+    public Result validatePhone(@RequestBody User user) {
+        if(!ValidateUtil.validateMobile(user.getPhone())) {
+            return Result.failResult("手机号码格式不正确");
+        }
+        User u = null;
+        try {
+            u = userService.findById(user.getId());
+            Integer code = messageUtil.getSixRandNum();
+            u.setVerifyCode(code);
+            u.setExpireDate(jwtTokenUtil.generateExpirationDate(new Date(), 1800));
+            messageUtil.sendRegisterSMS(u.getPhone(), code);
+            userService.update(u);
+        } catch (Exception e) {
+            return Result.errorResult("发送验证码失败");
+        }
+        return Result.successResult("验证码发送成功!").add("userInfo",u);
+    }
+
+    /**
+     * 忘记用户密码
+     */
+    @PutMapping("/forgetPassword")
+    @ResponseBody
+    public Result forgetPassword(@RequestBody User user) {
+        // 通过userID查询用户信息
+        try{
+            User u = userService.findById(user.getId());
+            BCryptPasswordEncoder encoderPassword = new BCryptPasswordEncoder();
+            u.setPassword(encoderPassword.encode(user.getPassword()));
+            userService.update(u);
+            return Result.successResult().add("userInfo",u);
+        }catch (Exception e){
+            return Result.errorResult("重置密码失败!");
         }
     }
 }
